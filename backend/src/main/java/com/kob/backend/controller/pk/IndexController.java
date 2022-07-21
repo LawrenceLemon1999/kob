cee8b9fae8067a890d50e5b9ded978913844ade6
ChenYuhang <497069023@qq.com>
2022-07-22 01:45:29 +0800

    mysql这一节课结束

--- backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
@@ -5,9 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/pk/")//先匹配到/pk/
 public class IndexController {
-    @RequestMapping("index/")//然后匹配到index/
+    @RequestMapping("/")//匹配到/
     public String index(){
         return "pk/index.html";//返回的页面路径，在templates里面的pk包里的index.html文件
     }
